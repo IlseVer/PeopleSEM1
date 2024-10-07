d654da473543cc8b5babe7374f97b717e4dec92d
Bavo Ketels <bavo.ketels@gmail.com>
2024-10-07 10:39:00 +0200

    Showing people as paragraph

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -7,7 +7,9 @@ namespace PeopleManager.Ui.Mvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var people = GetPeople();
+
+            return View(people);
         }
 
         public IList<Person> GetPeople()

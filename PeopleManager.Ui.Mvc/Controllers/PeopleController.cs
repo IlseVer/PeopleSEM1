9d855d8c279921987f647645a96b4994bf93310c
Bavo Ketels <bavo.ketels@gmail.com>
2024-11-18 10:28:06 +0100

    Delete form with Route DeleteConfirmed

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -69,6 +69,35 @@ namespace PeopleManager.Ui.Mvc.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed([FromRoute] int id)
+        {
+            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _peopleManagerDbContext.People.Remove(person);
+
+            _peopleManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
 
